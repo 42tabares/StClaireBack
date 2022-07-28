d74291c27c6fc9201bb080a98656ef62485250b3
42Tabares <42tabares@gmail.com>
2022-07-27 21:32:54 -0500

    firs commit

---
+++ src/main/java/com/sofkau/hospital/StClaireApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StClaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StClaireApplication.class, args);
+	}
+
+}
